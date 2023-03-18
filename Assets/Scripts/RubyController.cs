f8418e4b9a78f007e78b306189b3f94773bd2c24
Carlos Guedes <carlosguedesss17@gmail.com>
2023-03-18 09:42:29 +0000

    a52198@Etapa_Character Controller and Keyboard Input

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -3,23 +3,35 @@ using UnityEngine.InputSystem;
 
 public class RubyController : MonoBehaviour
 {
+    [Range(0, 10)][SerializeField] float speed = 3f;
+
     private PlayerInput rubyPlayerController;
+    private Vector2 currentMovementInput;
 
     void Start()
     {
-
+        //QualitySettings.vSyncCount = 0;
+        //Application.targetFrameRate = 10;
     }
-
 
     void Update()
     {
+        /*float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
         Vector2 position = transform.position;
-        position.x = position.x + 0.1f;
+        position.x += 0.1f * horizontal;
+        position.y += 0.1f * vertical;
+        transform.position = position; */
+
+        Vector2 position = transform.position;
+        position.x += speed * currentMovementInput.x * Time.deltaTime;
+        position.y += speed * currentMovementInput.y * Time.deltaTime;
         transform.position = position;
     }
 
-    void OnMove()
+    private void OnMove(InputValue movementInput)
     {
-
+        currentMovementInput = movementInput.Get<Vector2>();
     }
 }
